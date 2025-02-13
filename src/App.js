a1668fb57d1683239f9033ccee4d509d56b92f97
uilacceb <qu6qu6g@gmail.com>
2025-02-13 16:35:28 -0500

    added instance consult

--- src/App.js
+++ src/App.js
@@ -11,28 +11,30 @@ import Navbar from './Components/Navbar/Navbar.js';
 import Landing_Page from './Components/Landing_Page/Landing_Page.js';
 import SignUp from './Components/Sign_up/Sign_up.js';
 import Login from './Components/Login/Login.js';
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultationBooking/InstantConsultation.js"
 
 // Function component for the main App
 function App() {
 
-  // Render the main App component
-  return (
-    <div className="App">
-      {/* Set up BrowserRouter for routing */}
-      <BrowserRouter>
-        {/* Display the Navbar component */}
-        <Navbar />
+    // Render the main App component
+    return (
+        <div className="App">
+            {/* Set up BrowserRouter for routing */}
+            <BrowserRouter>
+                {/* Display the Navbar component */}
+                <Navbar />
 
-        {/* Set up the Routes for different pages */}
-        <Routes>
+                {/* Set up the Routes for different pages */}
+                <Routes>
 
-          <Route path="/" element={<Landing_Page />} />
-          <Route path="Sign_up" element={<SignUp />} />
-          <Route path="Login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+                    <Route path="/" element={<Landing_Page />} />
+                    <Route path="Sign_up" element={<SignUp />} />
+                    <Route path="Login" element={<Login />} />
+                    <Route path="/instant-consultation" element={<InstantConsultation />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 // Export the App component as the default export
